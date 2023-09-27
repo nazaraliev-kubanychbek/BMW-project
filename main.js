2c505fc0001ac42cee70b1d7726c636ea26ebc54
nazaraliev-kubanychbek <kuba.duishobaevich@gmail.com>
2023-09-27 19:26:54 +0600

    add slider

--- main.js
+++ main.js
@@ -3,3 +3,13 @@
 $(".burger-btn").on("click", function(){
     $(".header-menu").toggleClass("header-menu-show");
   });
+
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        loop: true,
+    });
+  });
